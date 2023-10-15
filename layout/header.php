a176524250ae3e377202bd8c7352713ede2019d3
Damien Dubost <ddubost6998@gmail.com>
2023-10-15 21:52:53 +0200

    Ajout de la structure du site

---
+++ layout/header.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="fr">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <script src="https://cdn.tailwindcss.com?plugins=typography"></script>
+        <title>L'actu musicale c'est sur SoundScoop !</title>
+    </head>
+    <body class="bg-slate-300">
+        <header>
+            <?php require_once __DIR__ . '/nav.php'; ?>
+        </header>
