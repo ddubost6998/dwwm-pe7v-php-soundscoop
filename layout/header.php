e873ceba981f7e384a8e10c6ed19daef5f47e5df
Damien Dubost <ddubost6998@gmail.com>
2023-10-19 12:33:36 +0200

    Ajout de la navbar

--- layout/header.php
+++ layout/header.php
@@ -4,6 +4,8 @@
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <script src="https://cdn.tailwindcss.com?plugins=typography"></script>
+        <link href="https://cdnjs.cloudflare.com/ajax/libs/flowbite/1.8.1/flowbite.min.css" rel="stylesheet" />
+        <link rel="icon" type="image/png" sizes="32x32" href="../img/favicon_soundscoop.png">
         <title>L'actu musicale c'est sur SoundScoop !</title>
     </head>
     <body class="bg-slate-300">
