8b69e81398d396e3b6cc4fae4f51897da6480c33
Damien Dubost <ddubost6998@gmail.com>
2023-10-24 16:33:27 +0200

    Ajout du process de connexion

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # SoundScoop
 
-Ajout de la méthode statique Utils dans une classe
-
+## Ajout de la méthode statique Utils dans une classe
 Cette classe permet de rediriger l'utilisateur vers un emplacement donnée
+
+## Dans login_process.php
+Je vérifie si la requête HTTP est une requête POST et redirige vers login.php si la condition n'est pas satisfaite.
--- login_process.php
+++ login_process.php
@@ -2,3 +2,26 @@
 
 require_once 'classes/Utils.php';
 
+if ($_SERVER['REQUEST_METHOD'] !== 'POST') {
+    Utils::redirect('login.php');
+}
+
+try {
+    $pdo = getConnection();
+
+    $email = $_POST['email'];
+    $password = $_POST['password'];
+    
+    $hashedPassword = password_hash($password, PASSWORD_DEFAULT);
+
+    $query = "INSERT INTO users (`email`, `password`) VALUES (:email, :hashedPassword)";
+    $stmtInsert = $pdo->prepare($query);
+
+    $stmtInsert->execute([
+        'email'          => $email,
+        'hashedPassword' => $hashedPassword
+    ]);
+    Utils::redirect('register_success.php');
+} catch (PDOException) {
+    Utils::redirect('register.php?error=' . AppError::DB_CONNECTION);
+}
