021fd2b9db10d5501951c326758081849257bc55
Damien Dubost <ddubost6998@gmail.com>
2023-10-25 16:53:11 +0200

    Modification du login_process.php

--- login_process.php
+++ login_process.php
@@ -1,27 +1,42 @@
 <?php
-
+require_once 'classes/ErrorEmails.php';
 require_once 'classes/Utils.php';
+require_once 'functions/db.php';
 
-if ($_SERVER['REQUEST_METHOD'] !== 'POST') {
+
+if (!isset($_POST['email']) || !isset($_POST['password'])) {
     Utils::redirect('login.php');
 }
 
+[
+    'email' => $email,
+    'password' => $password
+] = $_POST;
+
 try {
     $pdo = getConnection();
-
-    $email = $_POST['email'];
-    $password = $_POST['password'];
-    
-    $hashedPassword = password_hash($password, PASSWORD_DEFAULT);
-
-    $query = "INSERT INTO users (`email`, `password`) VALUES (:email, :hashedPassword)";
-    $stmtInsert = $pdo->prepare($query);
-
-    $stmtInsert->execute([
-        'email'          => $email,
-        'hashedPassword' => $hashedPassword
-    ]);
-    Utils::redirect('login_success.php');
 } catch (PDOException) {
     Utils::redirect('login.php?error=' . AppError::DB_CONNECTION);
 }
+
+$query = "SELECT * FROM users WHERE email = ?";
+
+$connectStmt = $pdo->prepare($query);
+$connectStmt->execute([$email]);
+
+$user = $connectStmt->fetch();
+
+if ($user === false) {
+    Utils::redirect('login.php?error=' . AppError::USER_NOT_FOUND);
+}
+
+if (!password_verify($password, $user['password'])) {
+    Utils::redirect('login.php?error=' . AppError::INVALID_CREDENTIALS);
+}
+
+$_SESSION['userInfos'] = [
+    'id' => $user['id'],
+    'email' => $email
+];
+
+Utils::redirect('admin.php');
