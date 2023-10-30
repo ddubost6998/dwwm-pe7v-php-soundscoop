760f9787ab59a852a6d5cd8fc75cdcac0d5c1a8f
Damien Dubost <ddubost6998@gmail.com>
2023-10-30 09:34:41 +0100

    Modification de la navbar pour rendre les liens active ou inactive

--- layout/nav.php
+++ layout/nav.php
@@ -1,3 +1,14 @@
+<?php
+require_once __DIR__ . '/../classes/MenuItem.php';
+
+$menuItems = [
+    new MenuItem('index.php', 'Accueil'),
+    new MenuItem('articles.php', 'Articles'),
+    new MenuItem('categories.php', 'Catégories'),
+    new MenuItem('about.php', 'À propos'),
+    new MenuItem('contact.php', 'Contact')
+];
+?>
 
 <nav class="fixed w-full bg-purple-50 z-20 top-0 left-0 shadow-lg dark:bg-purple-950">
     <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
@@ -8,21 +19,13 @@
 
         <div class="items-center justify-between hidden w-full md:flex md:w-auto md:order-1" id="navbar-sticky">
             <ul class="flex flex-col p-4 mt-4 font-semibold md:p-0 md:flex-row md:space-x-8 md:mt-0 md:border-0 dark:bg-gray-800 dark:border-gray-700">
-            <li>
-                <a href="index.php" class="block py-2 pl-3 pr-4 text-white bg-purple-700 rounded md:bg-transparent md:text-purple-700 md:p-0 md:dark:text-purple-500">Accueil</a>
-            </li>
-            <li>
-                <a href="articles.php" class="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Articles</a>
-            </li>
-            <li>
-                <a href="categories.php" class="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Catégories</a>
-            </li>
-            <li>
-                <a href="about.php" class="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">À propos</a>
-            </li>
-            <li>
-                <a href="contact.php" class="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Contact</a>
-            </li>
+                <?php foreach ($menuItems as $item) { ?>
+                <li>
+                    <a href="<?php echo $item->getUrl(); ?>" class="block py-2 pl-3 pr-4 rounded md:p-0 <?php echo $item->getCssClasses(); ?>">
+                    <?php echo $item->getLabel(); ?>
+                    </a>
+                </li>
+                <?php } ?>
             </ul>
         </div>
     </div>
