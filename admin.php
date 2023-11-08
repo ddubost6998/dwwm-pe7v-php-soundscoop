cd61c0a97b61d7052431b016ad14f74db584145e
Damien Dubost <ddubost6998@gmail.com>
2023-11-08 12:03:48 +0100

    Ajout de add_article.php pour ajouter un article côté admin

--- admin.php
+++ admin.php
@@ -29,6 +29,26 @@ $articles = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
     <a href="?logout=1">Déconnexion</a>
 
+    <h2>Ajouter un nouvel article</h2>
+    <form action="add_article.php" method="post">
+        <label for="title">Titre de l'article</label>
+        <input type="text" name="title" id="title" required>
+
+        <label for="content">Contenu de l'article</label>
+        <textarea name="content" id="content" rows="4" required></textarea>
+
+        <label for="issue_date">Date de publication</label>
+        <input type="date" name="issue_date" id="issue_date" required>
+
+        <label for="user_id">ID de l'utilisateur</label>
+        <input type="number" name="user_id" id="user_id" required>
+
+        <label for="url_img">URL de l'image</label>
+        <input type="text" name="url_img" id="url_img" required>
+
+        <button type="submit">Ajouter l'article</button>
+    </form>
+
     <table>
         <?php foreach ($articles as $article) { ?>
         <tr>
