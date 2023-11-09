5df2e7e6255110c3e4ac560a85364f9305ad75e6
Damien Dubost <ddubost6998@gmail.com>
2023-11-09 13:36:52 +0100

    Modification de la pagination

--- articles.php
+++ articles.php
@@ -59,9 +59,19 @@ if ($articles) { ?>
         <?php } ?>
 
         <div class="pagination text-center">
-            <?php for ($i = 1; $i <= $totalPages; $i++) { ?>
-                <a href="?page=<?php echo $i; ?>" class="<?php echo ($i === $page) ? 'active' : ''; ?>"><?php echo $i; ?></a>
-            <?php } ?>
+            <?php if ($totalPages > 1): ?>
+                <?php if ($page > 1): ?>
+                    <a href="?page=<?php echo ($page - 1); ?>" class="prev">Précédent</a>
+                <?php endif; ?>
+
+                <?php for ($i = 1; $i <= $totalPages; $i++): ?>
+                    <a href="?page=<?php echo $i; ?>" class="<?php echo ($i === $page) ? 'active' : ''; ?>"><?php echo $i; ?></a>
+                <?php endfor; ?>
+
+                <?php if ($page < $totalPages): ?>
+                    <a href="?page=<?php echo ($page + 1); ?>" class="next">Suivant</a>
+                <?php endif; ?>
+            <?php endif; ?>
         </div>
         
     </main>
